af24d8376d7448421d569b25832f85f39694b9f6
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2018-03-06 15:35:30 +0100

    Better feet sensor

--- src/it/unibo/oop17/ga_game/model/physics/B2DGroundEntityBody.java
+++ src/it/unibo/oop17/ga_game/model/physics/B2DGroundEntityBody.java
@@ -2,24 +2,24 @@ package it.unibo.oop17.ga_game.model.physics;
 
 import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.Fixture;
-import org.jbox2d.dynamics.contacts.ContactEdge;
 
 import it.unibo.oop17.ga_game.model.GroundEntityBody;
 import javafx.geometry.Dimension2D;
 import javafx.geometry.Point2D;
 
 /* package-protected */ final class B2DGroundEntityBody extends B2DBodyFacade implements GroundEntityBody {
-    private static final float FEET_SENSOR_HEIGHT = B2DUtils.EXTRA_SKIN_THICKNESS * 3 / 2; // a bit more than the skin
+    private static final float FEET_SENSOR_HEIGHT = B2DUtils.EXTRA_SKIN_THICKNESS;
     private final Fixture feetSensor;
 
     /* package-protected */ B2DGroundEntityBody(final Body body, final Dimension2D dimension,
             final B2DPhysicsEngine engine) {
         super(body, dimension);
 
+        // position the feet sensor to the bottom of the body
         feetSensor = new B2DFixtureBuilder()
-                .density(1)
+                .isSensor(true)
                 .rectangular(new Dimension2D(getDimension().getWidth(), FEET_SENSOR_HEIGHT))
-                .position(new Point2D(0, -getDimension().getHeight() / 2 + FEET_SENSOR_HEIGHT / 3))
+                .position(new Point2D(0, -getDimension().getHeight() / 2 + FEET_SENSOR_HEIGHT / 2))
                 .buildOn(body);
 
         // TODO migliora
@@ -28,12 +28,13 @@ import javafx.geometry.Point2D;
 
     @Override
     public boolean isOnGround() {
-        for (ContactEdge c = feetSensor.getBody().getContactList(); c != null; c = c.next) {
-            if (c.contact.isEnabled() && c.contact.isTouching() && B2DUtils.contains(c, feetSensor)) {
-                return true;
-            }
-        }
-        return false;
+        return B2DUtils.stream(getB2DBody().getContactList())
+                .filter(c -> c.contact.isEnabled())
+                .filter(c -> c.contact.isTouching())
+                .filter(c -> B2DUtils.contains(c, feetSensor)
+                        || c.contact.getManifold().localPoint.y >= getDimension().getHeight())
+                .findAny()
+                .isPresent();
     }
 
 }
