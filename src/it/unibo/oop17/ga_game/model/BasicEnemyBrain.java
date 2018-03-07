8e658cf7c7fc8a327b1e65e8ea479cefe105280e
Andrea Betti <andrea.betti10@studio.unibo.it>
2018-03-07 15:05:49 +0100

    creation static class for check position

--- src/it/unibo/oop17/ga_game/model/BasicEnemyBrain.java
+++ src/it/unibo/oop17/ga_game/model/BasicEnemyBrain.java
@@ -1,42 +1,28 @@
 package it.unibo.oop17.ga_game.model;
 
+import it.unibo.oop17.ga_game.utils.PositionCompare;
 import javafx.geometry.HorizontalDirection;
-import javafx.scene.shape.Rectangle;
 
 public class BasicEnemyBrain extends AbstractEntityComponent implements Brain {
 
     @Override
     public void beginContact(EntityBody other) {
 
-        final double ownerX = getEntity().getBody().getPosition().getX()
-                - getEntity().getBody().getDimension().getWidth() / 2;
-        final double ownerY = getEntity().getBody().getPosition().getY()
-                + getEntity().getBody().getDimension().getHeight() / 2;
-        final double ownerW = getEntity().getBody().getDimension().getWidth();
-        final double ownerH = getEntity().getBody().getDimension().getHeight();
-        final double otherX = other.getPosition().getX() - other.getDimension().getWidth() / 2;
-        final double otherY = other.getPosition().getY() + other.getDimension().getHeight() / 2;
-        final double otherW = other.getDimension().getWidth();
-        final double otherH = other.getDimension().getHeight();
-
-        // final Rectangle ownerR = new Rectangle(ownerX, ownerY, ownerW, ownerH);
-        final Rectangle otherR = new Rectangle(otherX, otherY, otherW, otherH);
-
         /*
          * in teoria dovrebbe cambiare direzione se il rettangolo di other contiene:
          * un punto leggermente distante dal vertice più in basso a destra dell'owner
          * un punto leggermente distante dal vertice più in basso a sinistra dell'owner
          */
-        if ((otherR.contains(ownerX + ownerW + 0.1, ownerY - ownerH + 0.1)
-                || otherR.contains(ownerX - 0.1, ownerY - ownerH + 0.1))
+        if ((PositionCompare.contactLeft(getEntity().getBody(), other)
+                || PositionCompare.contactRight(getEntity().getBody(), other))
                 && getEntity() instanceof BasicEnemy) {
-                final BasicEnemy x = (BasicEnemy) getEntity();
-                if (x.getMovingDirection().equals(HorizontalDirection.LEFT)) {
+            final BasicEnemy e = (BasicEnemy) getEntity();
+            if (e.getMovingDirection().equals(HorizontalDirection.LEFT)) {
                 System.out.println("right");
-                    x.move(HorizontalDirection.RIGHT);
+                e.move(HorizontalDirection.RIGHT);
                 } else {
                 System.out.println("left");
-                    x.move(HorizontalDirection.LEFT);
+                e.move(HorizontalDirection.LEFT);
                 }
         }
 
---
+++ src/it/unibo/oop17/ga_game/utils/PositionCompare.java
@@ -0,0 +1,60 @@
+package it.unibo.oop17.ga_game.utils;
+
+import it.unibo.oop17.ga_game.model.EntityBody;
+import javafx.scene.shape.Rectangle;
+
+public final class PositionCompare {
+
+    private static double ownerX;
+    private static double ownerY;
+    private static double ownerW;
+    private static double ownerH;
+    private static double otherX;
+    private static double otherY;
+    private static double otherW;
+    private static double otherH;
+
+    private static Rectangle otherR;
+
+    private PositionCompare() {
+
+    }
+
+    public static boolean contactRight(EntityBody owner, EntityBody other) {
+        makeRectangles(owner, other);
+        return otherR.contains(ownerX + ownerW + 0.1, ownerY - ownerH + 0.1)
+                || otherR.contains(ownerX + ownerW + 0.1, ownerY);
+    }
+
+    public static boolean contactLeft(EntityBody owner, EntityBody other) {
+        makeRectangles(owner, other);
+        return otherR.contains(ownerX - 0.1, ownerY - ownerH + 0.1) || otherR.contains(ownerX - 0.1, ownerY);
+    }
+
+    public static boolean contactUp(EntityBody owner, EntityBody other) {
+        makeRectangles(owner, other);
+        return otherR.contains(ownerX, ownerY + 0.1) || otherR.contains(ownerX + ownerW, ownerY + 0.1);
+    }
+
+    public static boolean contactDown(EntityBody owner, EntityBody other) {
+        makeRectangles(owner, other);
+        return otherR.contains(ownerX, ownerY - ownerH - 0.1)
+                || otherR.contains(ownerX + ownerW, ownerY - ownerH - 0.1);
+    }
+
+    private static void makeRectangles(final EntityBody owner, final EntityBody other) {
+        ownerX = owner.getPosition().getX()
+                - owner.getDimension().getWidth() / 2;
+        ownerY = owner.getPosition().getY()
+                + owner.getDimension().getHeight() / 2;
+        ownerW = owner.getDimension().getWidth();
+        ownerH = owner.getDimension().getHeight();
+        otherX = other.getPosition().getX() - other.getDimension().getWidth() / 2;
+        otherY = other.getPosition().getY() + other.getDimension().getHeight() / 2;
+        otherW = other.getDimension().getWidth();
+        otherH = other.getDimension().getHeight();
+
+        otherR = new Rectangle(otherX, otherY, otherW, otherH);
+    }
+
+}
