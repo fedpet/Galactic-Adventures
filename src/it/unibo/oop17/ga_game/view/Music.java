97b238aa346f9e16c33ecc55079a96cfac68ffe7
alessandroliva97 <alessandro.oliva@live.it>
2018-03-09 19:08:02 +0100

    Fix Music

--- src/it/unibo/oop17/ga_game/view/Music.java
+++ src/it/unibo/oop17/ga_game/view/Music.java
@@ -1,23 +1,23 @@
 package it.unibo.oop17.ga_game.view;
 
-import javafx.scene.media.AudioClip;
+import java.io.File;
 
 public enum Music {
     
-    TRACK1(new AudioClip("file:res/audio/music/action1.mp3")),
-    TRACK2(new AudioClip("file:res/audio/music/action2.mp3")),
-    TRACK3(new AudioClip("file:res/audio/music/generic1.mp3")),
-    TRACK4(new AudioClip("file:res/audio/music/generic2.mp3")),
-    TRACK5(new AudioClip("file:res/audio/music/generic4.mp3")),
-    TRACK6(new AudioClip("file:res/audio/music/generic5.mp3"));
+    TRACK1(new File("res/audio/music/action1.mp3")),
+    TRACK2(new File("res/audio/music/action2.mp3")),
+    TRACK3(new File("res/audio/music/generic1.mp3")),
+    TRACK4(new File("res/audio/music/generic2.mp3")),
+    TRACK5(new File("res/audio/music/generic4.mp3")),
+    TRACK6(new File("res/audio/music/generic5.mp3"));
     
-    private AudioClip clip;
+    private File media;
         
-    private Music(final AudioClip clip) {
-        this.clip = clip;
+    private Music(final File media) {
+        this.media = media;
     }
         
-    public AudioClip getMusic() {
-        return this.clip;
+    public String getMusic() {
+        return this.media.toURI().toString();
     }
 }
