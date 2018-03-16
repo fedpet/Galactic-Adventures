22b987d179453b852c903b9a895b52cdee58814d
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2018-03-16 22:34:52 +0100

    Add damage knockback

--- src/it/unibo/oop17/ga_game/model/entities/components/PlayerBrain.java
+++ src/it/unibo/oop17/ga_game/model/entities/components/PlayerBrain.java
@@ -4,6 +4,8 @@ import it.unibo.oop17.ga_game.model.physics.BodyContact;
 import javafx.geometry.Point2D;
 
 public class PlayerBrain extends AbstractBrain {
+    private static final double KNOCKBACK = 60;
+
     @Override
     public void update(final double dt) {
 
@@ -14,8 +16,8 @@ public class PlayerBrain extends AbstractBrain {
         contact.getOtherBody().getOwner().ifPresent(otherEntity -> {
             if (hate(otherEntity)
                     && contact.getPoint().getY() <= -getEntity().getBody().getDimension().getHeight() / 2) {
-                otherEntity.getLife().hurt(1);
-                getEntity().getBody().applyImpulse(new Point2D(0, 60));
+                // otherEntity.getLife().hurt(1);
+                getEntity().getBody().applyImpulse(new Point2D(0, KNOCKBACK));
             }
         }); 
     }
