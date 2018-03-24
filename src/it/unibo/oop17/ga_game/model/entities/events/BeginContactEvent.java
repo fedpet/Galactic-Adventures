a419015a5b178f884e40563751a80ce8a3d59b23
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2018-03-24 10:25:44 +0100

    Add doc

--- src/it/unibo/oop17/ga_game/model/entities/events/BeginContactEvent.java
+++ src/it/unibo/oop17/ga_game/model/entities/events/BeginContactEvent.java
@@ -3,7 +3,18 @@ package it.unibo.oop17.ga_game.model.entities.events;
 import it.unibo.oop17.ga_game.model.entities.Entity;
 import it.unibo.oop17.ga_game.model.physics.BodyContact;
 
+/**
+ * Generated at the beginning of a Contact.
+ */
 public final class BeginContactEvent extends ContactEvent {
+
+    /**
+     * 
+     * @param source
+     *            Entity generating the event.
+     * @param contact
+     *            The @BodyContact
+     */
     public BeginContactEvent(final Entity source, final BodyContact contact) {
         super(source, contact);
     }
--- src/it/unibo/oop17/ga_game/model/entities/events/EndContactEvent.java
+++ src/it/unibo/oop17/ga_game/model/entities/events/EndContactEvent.java
@@ -3,7 +3,17 @@ package it.unibo.oop17.ga_game.model.entities.events;
 import it.unibo.oop17.ga_game.model.entities.Entity;
 import it.unibo.oop17.ga_game.model.physics.BodyContact;
 
+/**
+ * Generated at the end of a contact.
+ */
 public final class EndContactEvent extends ContactEvent {
+    /**
+     * 
+     * @param source
+     *            Entity generating the event.
+     * @param contact
+     *            The @BodyContact
+     */
     public EndContactEvent(final Entity source, final BodyContact contact) {
         super(source, contact);
     }
