d839b7408b62754530d272dc0660a4d5fd272e91
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2018-03-29 18:47:06 +0200

    Improve doc

--- src/it/unibo/oop17/ga_game/model/physics/B2DBodySpawner.java
+++ src/it/unibo/oop17/ga_game/model/physics/B2DBodySpawner.java
@@ -5,7 +5,7 @@ import org.jbox2d.dynamics.BodyDef;
 import javafx.geometry.Dimension2D;
 
 /**
- * Provides a method to convert Box2D's Body to @B2DBodyFacade.
+ * Provides a method to convert Box2D's BodyDef to @B2DBodyFacade.
  */
 /* package-protected */ interface B2DBodySpawner {
     B2DBodyFacade spawn(BodyDef bodyDef, Dimension2D size);
