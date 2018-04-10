955e49fd048b87207df55e99c94eb379262eb913
alessandroliva97 <alessandro.oliva@live.it>
2018-04-10 21:08:20 +0200

    Change solid ground loading

--- src/it/unibo/oop17/ga_game/controller/LoadLevelImpl.java
+++ src/it/unibo/oop17/ga_game/controller/LoadLevelImpl.java
@@ -1,11 +1,11 @@
 package it.unibo.oop17.ga_game.controller;
 
 import java.util.LinkedHashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import org.mapeditor.core.Map;
 import org.mapeditor.core.ObjectGroup;
-import org.mapeditor.core.Tile;
 import org.mapeditor.core.TileLayer;
 
 import it.unibo.oop17.ga_game.model.CircleIterator;
@@ -24,14 +24,15 @@ import it.unibo.oop17.ga_game.model.entities.SlimeEnemy;
 import it.unibo.oop17.ga_game.model.physics.BodyFactory;
 import it.unibo.oop17.ga_game.utils.FXUtils;
 import it.unibo.oop17.ga_game.utils.InfiniteSequence;
-import it.unibo.oop17.ga_game.utils.SimpleCollisionGrid;
 import it.unibo.oop17.ga_game.view.GameWorldView;
 import javafx.geometry.Dimension2D;
 import javafx.geometry.Point2D;
+import javafx.util.Pair;
 
 public class LoadLevelImpl implements LoadLevel {
     
     private Player player;
+    private final Map map;
     private final GameWorld model;
     private final GameWorldView view;
     private final Set<EntityController> entities;
@@ -39,99 +40,110 @@ public class LoadLevelImpl implements LoadLevel {
 
     public LoadLevelImpl(final Map map, final GameWorld model, final GameWorldView view, final MainController mainController) {
         
+        this.map = map;
         this.model = model;
         this.view = view;
         this.entities = new LinkedHashSet<>();
         this.mainController = mainController;
-        map.forEach(layer -> {
+        this.map.forEach(layer -> {
             if (layer instanceof TileLayer) {
-                loadTerrain((TileLayer) layer);
+                loadTiles((TileLayer) layer);
             } else if (layer instanceof ObjectGroup) {
-                loadEntities((ObjectGroup) layer);
+                loadObjects((ObjectGroup) layer);
             }
         });
         
     }
 
-    private void loadEntities(final ObjectGroup layer) {
+    private void loadObjects(final ObjectGroup layer) {
         final BodyFactory bodyFactory = model.bodyFactory();
-        layer.forEach(mapObj -> {
-            final Point2D position = FXUtils.invertY(new Point2D(mapObj.getX() / 70, mapObj.getY() / 70));
-            final String type = mapObj.getType();
-            switch (type) {
-            case "player":
-                player = new Player(bodyFactory, position);
-                model.addEntity(player);
-                entities.add(new PlayerController(view.getPlayerInput(), player, view.entityFactory().createPlayer()));
-                break;
-            case "keyR":
-                final Key keyR = new Key(bodyFactory, position, KeyLockType.RED);
-                model.addEntity(keyR);
-                entities.add(new LifelessEntityController(keyR, view.entityFactory().createKey(KeyLockType.RED)));
-                break;
-            case "redLock":
-                final Lock lockR = new Lock(bodyFactory, position, KeyLockType.RED);
-                model.addEntity(lockR);
-                entities.add(new LifelessEntityController(lockR, view.entityFactory().createLock(KeyLockType.RED)));
-                break;
-            case "lever":
-                final Lever lever = new Lever(bodyFactory, position, "door", false);
-                model.addEntity(lever);
-                entities.add(new TriggerEntityController(lever, view.entityFactory().createLever()));
-                break;
-            case "door":
-                final Door door = new Door(bodyFactory, position, "door", false);
-                model.addEntity(door);
-                entities.add(new TriggerEntityController(door, view.entityFactory().createDoor()));
-                door.register(new DoorEventListener(this.mainController));
-                break;
-            case "gCoin":
-                final Coin gCoin = new Coin(bodyFactory, position, 100);
-                model.addEntity(gCoin);
-                entities.add(new LifelessEntityController(gCoin, view.entityFactory().createCoin(CoinType.GOLD)));
-                break;
-            case "sCoin":
-                final Coin sCoin = new Coin(bodyFactory, position, 50);
-                model.addEntity(sCoin);
-                entities.add(new LifelessEntityController(sCoin, view.entityFactory().createCoin(CoinType.SILVER)));
-                break;
-            case "bCoin":
-                final Coin bCoin = new Coin(bodyFactory, position, 25);
-                model.addEntity(bCoin);
-                entities.add(new LifelessEntityController(bCoin, view.entityFactory().createCoin(CoinType.BRONZE)));
-                break;
-            case "flying":
-                final FlyingEnemy flying = new FlyingEnemy(bodyFactory, position, InfiniteSequence
-                        .repeat(() -> new CircleIterator(position, 5, 5)));
-                model.addEntity(flying);
-                entities.add(new LivingEntityController(flying, view.entityFactory().createBee()));
-                break;
-            case "slime":
-            default:
-                final SlimeEnemy slime = new SlimeEnemy(bodyFactory, position);
-                model.addEntity(slime);
-                entities.add(new LivingEntityController(slime, view.entityFactory().createSlime()));
-                break;
-            }
-        });
-    }
+        final ObjectGroup objLayer = (ObjectGroup) layer;
+        if (objLayer.getName().trim().toLowerCase(Locale.UK).equals("solid")) {
+            objLayer.forEach(obj -> {
+                final Pair<Point2D, Dimension2D> pos = mapPositionToWorld(this.map, obj.getX(), obj.getY(),
+                        obj.getWidth(), obj.getHeight());
 
-    private void loadTerrain(final TileLayer layer) {
-        view.showTerrain(layer, Point2D.ZERO,
-                new Dimension2D(ModelSettings.TILE_SIZE, ModelSettings.TILE_SIZE));
-        if (layer.getName().equals("ground")) {
-            model.bodyFactory()
-            .createTerrainFromGrid(Point2D.ZERO,
-                    new Dimension2D(ModelSettings.TILE_SIZE, ModelSettings.TILE_SIZE),
-                    new SimpleCollisionGrid(layer.getMap().getHeight(), layer.getMap().getWidth(),
-                            (row, column) -> {
-                                return doesTileExists(layer.getTileAt(column, row));
-                            }));
+                model.bodyFactory().createStatic(pos.getKey(), pos.getValue());
+            });
+        }
+        if (layer.getName().trim().toLowerCase(Locale.UK).equals("objects")) {
+            layer.forEach(mapObj -> {
+                final Point2D position = FXUtils.invertY(new Point2D(mapObj.getX() / 70, mapObj.getY() / 70));
+                final String type = mapObj.getType();
+                switch (type) {
+                case "player":
+                    player = new Player(bodyFactory, position);
+                    model.addEntity(player);
+                    entities.add(
+                            new PlayerController(view.getPlayerInput(), player, view.entityFactory().createPlayer()));
+                    break;
+                case "keyR":
+                    final Key keyR = new Key(bodyFactory, position, KeyLockType.RED);
+                    model.addEntity(keyR);
+                    entities.add(new LifelessEntityController(keyR, view.entityFactory().createKey(KeyLockType.RED)));
+                    break;
+                case "redLock":
+                    final Lock lockR = new Lock(bodyFactory, position, KeyLockType.RED);
+                    model.addEntity(lockR);
+                    entities.add(new LifelessEntityController(lockR, view.entityFactory().createLock(KeyLockType.RED)));
+                    break;
+                case "lever":
+                    final Lever lever = new Lever(bodyFactory, position, "door", false);
+                    model.addEntity(lever);
+                    entities.add(new TriggerEntityController(lever, view.entityFactory().createLever()));
+                    break;
+                case "door":
+                    final Door door = new Door(bodyFactory, position, "door", false);
+                    model.addEntity(door);
+                    entities.add(new TriggerEntityController(door, view.entityFactory().createDoor()));
+                    door.register(new DoorEventListener(this.mainController));
+                    break;
+                case "gCoin":
+                    final Coin gCoin = new Coin(bodyFactory, position, 100);
+                    model.addEntity(gCoin);
+                    entities.add(new LifelessEntityController(gCoin, view.entityFactory().createCoin(CoinType.GOLD)));
+                    break;
+                case "sCoin":
+                    final Coin sCoin = new Coin(bodyFactory, position, 50);
+                    model.addEntity(sCoin);
+                    entities.add(new LifelessEntityController(sCoin, view.entityFactory().createCoin(CoinType.SILVER)));
+                    break;
+                case "bCoin":
+                    final Coin bCoin = new Coin(bodyFactory, position, 25);
+                    model.addEntity(bCoin);
+                    entities.add(new LifelessEntityController(bCoin, view.entityFactory().createCoin(CoinType.BRONZE)));
+                    break;
+                case "flying":
+                    final FlyingEnemy flying = new FlyingEnemy(bodyFactory, position, InfiniteSequence
+                            .repeat(() -> new CircleIterator(position, 5, 5)));
+                    model.addEntity(flying);
+                    entities.add(new LivingEntityController(flying, view.entityFactory().createBee()));
+                    break;
+                case "slime":
+                default:
+                    final SlimeEnemy slime = new SlimeEnemy(bodyFactory, position);
+                    model.addEntity(slime);
+                    entities.add(new LivingEntityController(slime, view.entityFactory().createSlime()));
+                    break;
+                }
+            });
         }
     }
 
-    private boolean doesTileExists(final Tile tile) {
-        return tile != null && tile.getImage() != null;
+    private void loadTiles(final TileLayer layer) {
+        view.showTerrain(layer, Point2D.ZERO,
+                new Dimension2D(ModelSettings.TILE_SIZE, ModelSettings.TILE_SIZE));
+    }
+    
+    private Pair<Point2D, Dimension2D> mapPositionToWorld(final Map map,
+            final double x, final double y,
+            final double width, final double height) {
+        final Dimension2D dim = new Dimension2D(width / map.getTileWidth(),
+                height / map.getTileHeight());
+        final Point2D pos = new Point2D(x / map.getTileWidth() + dim.getWidth() / 2,
+                -(y / map.getTileHeight() + dim.getHeight() / 2));
+
+        return new Pair<>(pos, dim);
     }
     
     @Override
