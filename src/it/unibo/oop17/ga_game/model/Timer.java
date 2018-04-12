57fe0766e95eddc13f42dc27302b27202a278fec
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2018-04-12 20:45:22 +0200

    Revert "Wrong branch"
    This reverts commit e56f2925c5f8294c55ba9e143a6dc482d2d6bd77.

---
+++ src/it/unibo/oop17/ga_game/model/Timer.java
@@ -0,0 +1,35 @@
+package it.unibo.oop17.ga_game.model;
+
+/**
+ * A timer keeps track of time.
+ */
+public interface Timer {
+    /**
+     * Restarts the timer.
+     */
+    void restart();
+
+    /**
+     * Acknowledge the passage of time.
+     * 
+     * @param dt
+     *            Amount of time passed in seconds
+     */
+    void update(double dt);
+
+    /**
+     * @return true if the timer is elapsed.
+     */
+    boolean isElapsed();
+
+    /**
+     * Static factory for a timer working with seconds.
+     * 
+     * @param time
+     *            in seconds
+     * @return A @Timer.
+     */
+    static Timer seconds(final double time) {
+        return new TimerImpl(time);
+    }
+}
