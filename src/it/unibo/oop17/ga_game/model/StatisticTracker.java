6ac55bbf8c0a19a99d60c4ffc6a7c408e081a303
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2018-04-13 17:45:54 +0200

    Add statistics

---
+++ src/it/unibo/oop17/ga_game/model/StatisticTracker.java
@@ -0,0 +1,60 @@
+package it.unibo.oop17.ga_game.model;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+
+import com.google.common.eventbus.Subscribe;
+
+import it.unibo.oop17.ga_game.model.entities.Entity;
+import it.unibo.oop17.ga_game.model.entities.components.Inventory;
+import it.unibo.oop17.ga_game.model.entities.events.DestructionEvent;
+import it.unibo.oop17.ga_game.model.entities.events.EntityEventListener;
+import it.unibo.oop17.ga_game.model.entities.events.HitEvent;
+
+/**
+ * Tracks statistics of an @Entity.
+ */
+public final class StatisticTracker implements EntityStatistic {
+    private final Entity trackedEntity;
+    private int numberOfEnemiesKilled;
+
+    /**
+     * @param entity
+     *            The entity to be tracked.
+     */
+    public StatisticTracker(final Entity entity) {
+        trackedEntity = Objects.requireNonNull(entity);
+        trackedEntity.register(new MyEntityListener());
+    }
+
+    @Override
+    public int getMoneyCollected() {
+        return trackedEntity.get(Inventory.class).isPresent() ? trackedEntity.get(Inventory.class).get().getMoney() : 0;
+    }
+
+    @Override
+    public Set<KeyLockType> getKeysCollected() {
+        return trackedEntity.get(Inventory.class).isPresent() ? trackedEntity.get(Inventory.class).get().getKeysBunch()
+                : Collections.emptySet();
+    }
+
+    @Override
+    public int getEnemiesKilled() {
+        return numberOfEnemiesKilled;
+    }
+
+    private final class MyEntityListener implements EntityEventListener {
+        @Subscribe
+        public void onDestruction(final DestructionEvent event) {
+            event.getSource().unregister(this);
+        }
+
+        @Subscribe
+        public void onHit(final HitEvent event) {
+            if (event.hasKilled()) {
+                numberOfEnemiesKilled++;
+            }
+        }
+    }
+}
