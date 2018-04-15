a1505b84b999fea35e31b3381caeb6940fad200c
alessandroliva97 <alessandro.oliva@live.it>
2018-04-15 14:40:23 +0200

    fix import

--- src/it/unibo/oop17/ga_game/controller/GameControllerImpl.java
+++ src/it/unibo/oop17/ga_game/controller/GameControllerImpl.java
@@ -1,8 +1,6 @@
 package it.unibo.oop17.ga_game.controller;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedHashSet;
